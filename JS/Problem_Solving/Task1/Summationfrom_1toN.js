a04158053189e6250b633667304e754a99033f41
Suhaila Mohamed <125570330+suhailamohamed21@users.noreply.github.com>
2024-03-21 03:35:05 +0200

    Delete JS/Problem_Solving directory

--- JS/Problem_Solving/Task1/Summationfrom_1toN.js
+++
@@ -1,8 +0,0 @@
-function summationOfNumbers(N) {
-    if(N > 1)
-        return (N + summationOfNumbers(N - 1));
-    else
-        return 1;
-}
-
-console.log("Sum = " + summationOfNumbers(3));
--- JS/Problem_Solving/Task2/MyHeadIsAtTheWrongEnd!.js
+++
@@ -1,7 +0,0 @@
-function rearrangeArray(array) {
-    // Create a new array with elements in reverse order
-    return [array[2], array[1], array[0]];
-}
-
-const array = ['tail', 'body', 'head'];
-console.log(rearrangeArray(array)); 
